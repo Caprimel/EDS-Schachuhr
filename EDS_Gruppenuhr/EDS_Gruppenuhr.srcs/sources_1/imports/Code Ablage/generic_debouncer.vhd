020673251acdf31355e567829e35c8a4bb069fd9
Caprimel <spieliewheely@skymail.de>
2026-04-29 12:11:22 +0200

    debbouncer integration in progress

---
+++ EDS_Gruppenuhr/EDS_Gruppenuhr.srcs/sources_1/imports/Code Ablage/generic_debouncer.vhd
@@ -0,0 +1,42 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.numeric_std.all; 
+
+entity generic_debouncer is -- eher ein generic delay
+    generic (   signal_amount : integer;
+                signal_eq_len : integer;        -- wie viele aufeinanderfolgende Signale gleich sein sollen
+                delay : integer := 20000000);   -- 20 Mio Zyklen, bei 100 MHz --> 5 Hz --> 200 ms 
+    port(
+        Clk : in std_logic;
+        input : in unsigned(signal_amount-1 downto 0);
+        output: out unsigned(signal_amount-1 downto 0));
+end generic_debouncer;
+
+
+architecture Behavioral of generic_debouncer is
+    type internal_array_type is array (signal_amount-1 downto 0) of unsigned(signal_eq_len-1 downto 0);
+    signal Reg_new : internal_array_type;
+    signal Reg_old : internal_array_type;
+    
+--for loop außerhalb eines Prozesses    
+--für jedes Bit eine eigene debouncer Instanz mit Delay und Schieberegister
+GenDeb : for i in signal_amount-1 downto 0 generate
+    begin
+    LastSig: process(Clk) 
+        variable clock_counter : integer range 0 to delay := 0;
+        begin
+        if rising_edge(Clk) then
+            -- Schieberegister: neuen Wert von links reinschieben
+            Reg_new(i) <= input(i) & Reg_new(i)(signal_eq_len-1 downto 1);
+              
+            if (clock_counter < delay) then
+                clock_counter := clock_counter + 1;
+            end if;  
+            elsif (Reg_new(i) = Reg_old(i)) then
+                output(i) <= input(i);
+                clock_counter := 0;
+            end if;
+        end if;
+    end process LastSig;
+end generate;
+end Behavioral;  
